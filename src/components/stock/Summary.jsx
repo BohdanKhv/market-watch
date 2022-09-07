c4cc3fdc92e179e3592a1953ba16e480cf1a4036
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-09-07 19:48:49 -0400

    ellipse ui

--- src/components/stock/Summary.jsx
+++ src/components/stock/Summary.jsx
@@ -18,20 +18,24 @@ const Summary = () => {
     }, [portfolio])
 
     return (
-        <div className="flex-grow-sm-1">
+        <div className="flex-grow-sm-1 flex-b-75">
             <div className="box-content">
                 <div className="p-3 p-sm-75 flex flex-col gap-4 white-space-nowrap">
                     <div>
                         <h4 className="text-secondary weight-400">
                             P&L
                         </h4>
-                        <div className={`flex justify-between align-center pt-2 px-1 gap-4 ${pnl > 0 ? 'text-success' : pnl < 0 ? 'text-danger' : 'text-secondary' }`}>
-                            <h3 className="weight-400">
-                            {pnl > 0 ? '+ ' : pnl < 0 ? '- ' : ''}$ {addCommaToNumber(pnl)?.replace('-', '')}
-                            </h3>
-                            <h5 className="fs-12 weight-400">
-                            {percentPnl > 0 ? '+' : percentPnl < 0 ? '-' : ''} {percentPnl?.toString()?.replace('-', '')}%
-                            </h5>
+                        <div className={`flex flex-wrap justify-between align-center pt-2 px-1 ${pnl > 0 ? 'text-success' : pnl < 0 ? 'text-danger' : 'text-secondary' }`}>
+                            <div className="flex-b-65 w-min-0">
+                                <div className="fs-16 text-ellipsis">
+                                    {pnl > 0 ? '+ ' : pnl < 0 ? '- ' : ''}$ {addCommaToNumber(pnl)?.replace('-', '')}
+                                </div>
+                            </div>
+                            <div className="flex-b-35 w-min-0 text-end">
+                                <div className="fs-12 text-ellipsis">
+                                    {percentPnl > 0 ? '+' : percentPnl < 0 ? '-' : ''} {(+percentPnl)?.toFixed(2)?.toString()?.replace('-', '')}%
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div>
@@ -39,12 +43,16 @@ const Summary = () => {
                             Top Gain
                         </h4>
                         <div className="flex justify-between align-center pt-2 px-1">
-                            <h3 className="weight-400">
-                            {topGain?.item?.symbol}
-                            </h3>
-                            <h5 className="text-success pt-1 weight-400">
-                            {topGain.amount > 0 ? '+ ' : topGain.amount < 0 ? '- ' : ''}{addCommaToNumber(topGain.amount?.toFixed(2))?.replace('-', '')}
-                            </h5>
+                            <div className="flex-b-50 w-min-0">
+                                <div className="fs-16">
+                                    {topGain?.item?.symbol}
+                                </div>
+                            </div>
+                            <div className="flex-b-50 w-min-0 text-end">
+                                <div className="fs-12 text-ellipsis text-success">
+                                    {topGain.amount > 0 ? '+ ' : topGain.amount < 0 ? '- ' : ''}{addCommaToNumber(topGain.amount?.toFixed(0))?.replace('-', '')}
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div>
@@ -52,12 +60,16 @@ const Summary = () => {
                             Top Loss
                         </h4>
                         <div className="flex justify-between align-center pt-2 px-1">
-                            <h3 className="weight-400">
-                            {topLoss?.item?.symbol}
-                            </h3>
-                            <h5 className="text-danger pt-1 weight-400">
-                            {topLoss.amount > 0 ? '+ ' : topLoss.amount < 0 ? '- ' : ''}{addCommaToNumber(topLoss.amount?.toFixed(2))?.replace('-', '')}
-                            </h5>
+                            <div className="flex-b-50 w-min-0">
+                                <div className="fs-16">
+                                {topLoss?.item?.symbol}
+                                </div>
+                            </div>
+                            <div className="flex-b-50 w-min-0 text-end">
+                                <div className="fs-12 text-ellipsis text-danger">
+                                    {topLoss.amount > 0 ? '+ ' : topLoss.amount < 0 ? '- ' : ''}{addCommaToNumber(topLoss.amount?.toFixed(0))?.replace('-', '')}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/stock/Total.jsx
+++ src/components/stock/Total.jsx
@@ -12,14 +12,20 @@ const Total = () => {
     }, [portfolio])
 
     return (
-        <div className="flex-grow-sm-1">
+        <div className="flex-grow-sm-1 flex-b-50">
             <div className="box-content">
                 <div className="p-3 p-sm-75 flex flex-col gap-4 white-space-nowrap">
                     <div>
-                    <h4 className="text-secondary weight-400">
-                        Market Value
-                    </h4>
-                    <h2 className="pt-2 weight-400 px-1">$ {addCommaToNumber(totalPortfolioValue)}</h2>
+                        <h4 className="text-secondary weight-400">
+                            Market Value
+                        </h4>
+                        <div className="pt-2 px-1 grid">
+                            <div className="w-min-0">
+                                <div className="fs-16 text-ellipsis">
+                                    $ {addCommaToNumber(totalPortfolioValue)}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div>
                     <h4 className="text-secondary weight-400">
